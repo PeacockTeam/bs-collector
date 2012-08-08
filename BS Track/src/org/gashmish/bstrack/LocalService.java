d796c9a2d6e21226308d2aaea1571a53954ac9d1
mad <owner.mad.epa@gmail.com>
2012-08-08 13:04:30 +0400

    Первый коммит!

---
+++ BS Track/src/org/gashmish/bstrack/LocalService.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gashmish.bstrack;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Environment;
+import android.os.IBinder;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
+import android.util.Log;
+
+public class LocalService extends Service {
+
+	private final Timer timer = new Timer();
+	private TelephonyManager telephonyManager;
+	private BufferedWriter bufferedWriter;
+	
+	public class MyTimerTask extends TimerTask {
+		
+		@Override
+		public void run() {
+			
+			try {
+				GsmCellLocation gsmCellLocation =
+					(GsmCellLocation) telephonyManager.getCellLocation();
+								
+				if (gsmCellLocation != null) {
+					bufferedWriter.write(
+						(new Date()).toGMTString() + "," + 
+						gsmCellLocation.getLac() + "," + 
+						gsmCellLocation.getCid() + "\n");
+					bufferedWriter.flush();
+				}
+				
+				//Log.i("Timer", "timer executed");
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+    @Override
+    public void onCreate() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("LocalService", "onStartCommand()");
+
+        telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        bufferedWriter = createDumpFile();
+        
+        if (bufferedWriter != null && telephonyManager != null) {
+            timer.scheduleAtFixedRate(new MyTimerTask(), 0, 10000);
+        } else {
+        	Log.e("LocalService", "failed to init service");
+        }
+        
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i("LocalService", "onDestroy()");
+        
+        timer.cancel();
+    	
+        try {
+			bufferedWriter.close();
+		} catch (IOException e) {
+			Log.e("LocalService", "failed to close file");
+		}
+    }
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		Log.i("LocalService", "onBind()");
+		return null;
+	}
+		
+	protected BufferedWriter createDumpFile() {
+		String baseDir =
+			Environment.getExternalStorageDirectory().getAbsolutePath() +
+			File.separator +
+			"bs_track";
+		
+		Date d = new Date();
+		String dateString = String.format("%d.%d.%d-%d.%d.%d",
+			d.getYear(), d.getMonth(), d.getDay(), d.getHours(), d.getMinutes(), d.getSeconds());
+		
+		String fileName = "bs_dump_" + dateString + ".csv";
+		
+		String fullPath =
+			baseDir +
+			File.separator + 
+			fileName;
+		
+		try {
+			
+			(new File(baseDir)).mkdirs();
+			FileWriter fstream = new FileWriter(fullPath, true);
+			return new BufferedWriter(fstream);
+			
+		} catch (Exception e) {
+			
+			System.err.println("Error: " + e.getMessage());
+			return null;
+		}		
+	}
+}
+
---
+++ src/org/gashmish/bstrack/MainActivity.java
@@ -0,0 +1,54 @@
+package org.gashmish.bstrack;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        // Watch for button clicks.
+        Button button = (Button)findViewById(R.id.start);
+        button.setOnClickListener(mStartListener);
+        button = (Button)findViewById(R.id.stop);
+        button.setOnClickListener(mStopListener);
+        button = (Button)findViewById(R.id.create);
+        button.setOnClickListener(mCreateListener);
+    }
+
+    private OnClickListener mStartListener = new OnClickListener() {
+        public void onClick(View v) {
+            // Make sure the service is started.  It will continue running
+            // until someone calls stopService().  The Intent we use to find
+            // the service explicitly specifies our service component, because
+            // we want it running in our own process and don't want other
+            // applications to replace it.
+            startService(new Intent(MainActivity.this, LocalService.class));
+        }
+    };
+
+    private OnClickListener mStopListener = new OnClickListener() {
+        public void onClick(View v) {
+            // Cancel a previous call to startService().  Note that the
+            // service will not actually stop at this point if there are
+            // still bound clients.
+            stopService(new Intent(MainActivity.this, LocalService.class));
+        }
+    };
+    
+    private OnClickListener mCreateListener = new OnClickListener() {
+		
+		public void onClick(View arg0) {
+			
+		}
+	};
+}
